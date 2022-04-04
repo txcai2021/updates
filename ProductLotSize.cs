d36ac92071f277a07e2ccf63d4808e96fb9344f9
TR-RPS-Admin <tr-rps-admin@gmail.com>
2022-04-04 19:24:01 +0800

    update

---
+++ ProductLotSize.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SIMTech.APS.Scheduling.API.PresentationModels
+{
+
+    public class ProductLotSize
+    {
+        public string PartName { set; get; }
+
+        public int LotSize { set; get; }
+    }
+
+    public class ProductLotSizeRecommendation
+    {
+        public ProductLotSizeRecommendation()
+        {
+            LotSizeReport = new List<ProductLotSizeReport>();
+        }
+        public int TryLotSize { set; get; }
+
+        public int FinalRecommendation { set; get; }
+
+        public List<ProductLotSizeReport> LotSizeReport { set; get; }
+    }
+
+
+
+    public class ProductLotSizeReport
+    {
+        public int Iteration { get; set; }
+
+        public string PartName { set; get; }
+
+        public int LotSize { set; get; }
+
+        public int Tardiness { get; set; }
+    }
+}
