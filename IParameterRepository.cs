1fdc485e89a8f7fa3f4fb465b3823dd75933dc10
txcai2021 <tianxiang@gmail.com>
2022-06-28 15:03:10 +0800

    updates

---
+++ IParameterRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SIMTech.APS.Operation.API.Repository
+{
+    using SIMTech.APS.Operation.API.Models;
+    using SIMTech.APS.Repository;
+    public interface IParameterRepository : IRepository<Parameter>
+    {    
+    }
+
+}
---
+++ ParameterRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+
+
+namespace SIMTech.APS.Operation.API.Repository
+{
+    using SIMTech.APS.Operation.API.Models;
+    using SIMTech.APS.Operation.API.DBContext;
+    using SIMTech.APS.Repository;
+
+    public class ParameterRepository : Repository<Parameter>,  IParameterRepository
+    {
+        private readonly OperationContext _dbContext;
+        public ParameterRepository(OperationContext context) : base(context) { _dbContext = context; }
+    }
+}
