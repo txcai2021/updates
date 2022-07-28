c251f1c560ed706f22fb03ff9a8504e57da868b2
txcai2021 <tianxiang@gmail.com>
2022-07-28 16:05:06 +0800

    updates

---
+++ IResourceParameterRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SIMTech.APS.Resource.API.Repository
+{
+    using SIMTech.APS.Resource.API.Models;
+    using SIMTech.APS.Repository;
+    public interface IResourceParameterRepository : IRepository<EquipmentParameter>
+    {     
+                   
+    }
+}
